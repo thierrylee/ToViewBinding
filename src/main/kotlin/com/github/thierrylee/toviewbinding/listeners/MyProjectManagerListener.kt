6ba5ac1629bc4dc46a0faedb2cfd2df08a883812
GitHub Action <action@github.com>
2022-04-04 08:26:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/thierrylee/toviewbinding/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/thierrylee/toviewbinding/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.thierrylee.toviewbinding.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.thierrylee.toviewbinding.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/thierrylee/toviewbinding/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thierrylee/toviewbinding/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.thierrylee.toviewbinding.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thierrylee.toviewbinding.MyBundle
 
 class MyProjectService(project: Project) {
 
